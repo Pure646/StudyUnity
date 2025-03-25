1d724a3468b3b6875be8b402580ef9a5c14ca0b4
Pure646 <tkdtnsdl1598@naver.com>
2025-03-25 21:52:24 +0900

    [ 2025-03-25 ] 학원 수업 SceneManager.LoadScene onClick

--- Study_2025_03_18/Assets/Scripts/Title_Mgr.cs
+++ Study_2025_03_18/Assets/Scripts/Title_Mgr.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
+// { } 짝을 찾아가는 단축키 : <Ctrl> + ]
+// 글씨 크기 조절 : <Ctrl> + 마우스 휠
 public class Title_Mgr : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Button StartBtn;
+
+    private void Start()
     {
-        
+        if (StartBtn != null)
+        { 
+            StartBtn.onClick.AddListener(StartClick);
+        }
+    }
+    private void Update()
+    {
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartClick()
     {
-        
+        Debug.Log("게임 시작 버튼 클릭");
+        SceneManager.LoadScene("LobbyScene");
     }
 }

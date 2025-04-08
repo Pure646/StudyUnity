f0884b4ac7d04b7f6f870072c15f105dfd0118e0
Pure646 <tkdtnsdl1598@naver.com>
2025-04-08 19:51:02 +0900

    [ 2025-04-08 ] solo 코인 파괴 하면서 코인 들어오는 오류 수정(미완성)

--- Study_Solo_Unity/Assets/Scenes/Scripts/Coin_Scripts.cs
+++ Study_Solo_Unity/Assets/Scenes/Scripts/Coin_Scripts.cs
@@ -2,26 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
+using static UnityEditorInternal.ReorderableList;
 
 public class Coin_Scripts : MonoBehaviour
 {
     private Text UI_Text;
+    private GameObject textObject;
     private int coin;
     private void Awake()
     {
-        GameObject textObject = GameObject.Find("CoinText");
+        textObject = GameObject.Find("CoinText");
         if(textObject != null)
         {
             UI_Text = textObject.GetComponentInChildren<Text>();
         }
     }
+    private void Start()
+    {
+        Debug.Log(Default_Result.Instance.coin);
+    }
     private void OnDestroy()
     {
-        coin = 1;
-        if(Default_Result.Instance.Coin_Number < 1000)
+        if(UI_Text != null)
         {
-            //Default_Result.Instance.Coin_Number += coin;
-            UI_Text.text = " x " + Default_Result.Instance.Coin_Number.ToString("000");
+            Default_Result.Instance.coin++;
+            coin = Default_Result.Instance.coin;
+            UI_Text.text = " x " + coin.ToString("000");
         }
     }
 }
--- Study_Solo_Unity/Assets/Scenes/Scripts/Default_Result.cs
+++ Study_Solo_Unity/Assets/Scenes/Scripts/Default_Result.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Default_Result : MonoBehaviour
 {
-    public static Default_Result Instance { get; private set; }
+    public static Default_Result Instance
+    {
+        get;
+        private set;
+    }
     private Text cointext;
-    public int Coin_Number => coin;
-    private int coin;
+    public int coin = 0;
     private void Awake()
     {
         cointext = GetComponent<Text>();
+        if(Instance != null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
     private void Start()
     {
-        cointext.text = " x " + coin;
+        cointext.text = " x " + coin.ToString("000");
     }
 }

4994c20fe8b4e958f9c4af77ca8849e5e44e5d45
Pure646 <tkdtnsdl1598@naver.com>
2025-06-01 16:23:36 +0900

    [ 2025-06-01 ] StudyUnity 학원 과제 클리어

--- Study_2025_04_01/Assets/Study Data/Academy_assignment/CarRank/CarRank ScriptFolder/CarSystem.cs
+++ Study_2025_04_01/Assets/Study Data/Academy_assignment/CarRank/CarRank ScriptFolder/CarSystem.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarSystem : MonoBehaviour
 {
-    private float CarSpeed = 0;
+    public float CarSpeed;
+
     private Vector2 StartPos;
     private Vector2 EndPos;
-    private float StopCarSpeed = 0.0001f;
+    public bool stop;           // true ÀÌ¸é ¸ØÃá°ÍÀÌ°í ¾Æ´Ï¸é false
+
     private void Start()
     {
         Application.targetFrameRate = 60;
@@ -17,23 +20,34 @@ public class CarSystem : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            gameObject.transform.position = new Vector2(-7f, -3.65f);
             this.StartPos = Input.mousePosition;
         }
-        if(Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0)) 
         {
             this.EndPos = Input.mousePosition;
             float swipeLength = this.EndPos.x - this.StartPos.x;
             
             if(swipeLength > 0)
             {
-                this.CarSpeed = swipeLength / 700.0f;
+                this.CarSpeed = swipeLength / 1000.0f;
             }
             else
             {
                 this.CarSpeed = 0;
             }
         }
-        transform.Translate(this.CarSpeed, 0, 0);
-        this.CarSpeed *= 0.98f;
+
+        if (this.CarSpeed < 0.001f && stop == false)
+        {
+            stop = true;
+            this.CarSpeed = 0;
+        }
+        else if (this.CarSpeed >= 0.001f)
+        {
+            gameObject.transform.Translate(this.CarSpeed, 0, 0);
+            this.CarSpeed *= 0.98f;
+            stop = false;
+        }
     }
 }

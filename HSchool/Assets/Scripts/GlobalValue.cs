78b4fc0c7387a1e3fbf285f26ad18fdd03c2b0a6
Pure646 <tkdtnsdl1598@naver.com>
2025-08-06 19:06:03 +0900

    [ 2025-08-06 ]
    StudyUnity

--- HSchool/Assets/Scripts/GlobalValue.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GlobalValue : MonoBehaviour
-{
-    public static int g_BestScore;
-    public static int g_UserGold;
-}
